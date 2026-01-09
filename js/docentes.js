679c4d502fcf3325b6441e0a1d3ced33bf788297
Lamaruggine <marcelolama.27@gmail.com>
2026-01-09 01:15:43 -0300

    agregamos logica en .js para que los docentes se creen desde un arreglo

---
+++ js/docentes.js
@@ -0,0 +1,32 @@
+
+fetch("/data/docentes.json")
+.then(response => {
+    if(!response.ok){
+        throw new Error("No se pudo cargar el archivo JSON"); 
+    }
+    return response.json();
+})
+.then(docentes => {
+    console.log(docentes); 
+    mostrarDocentes(docentes); 
+})
+.catch(error => {
+    console.error("Error: ", error); 
+}); 
+
+function mostrarDocentes(docentes){
+    const contenedorDocentes = document.querySelector("#contDocentes")
+    
+    docentes.forEach(docente => {
+        let contenedor = document.createElement("div"); 
+        contenedor.innerHTML = `
+            <div class="cell">
+                    <img class="imagenDocente" src="${docente.imagen}" alt="">
+                    <p class="nombreDocente">${docente.nombre}</p>
+                    <p class="cargoDocente">${docente.cargo}</p>
+            </div>
+        ` 
+        contenedorDocentes.appendChild(contenedor); 
+    });
+    
+}
