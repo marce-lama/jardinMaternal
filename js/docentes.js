d2707f22d90c4b6a40348c9b8f0505ca6eb4f1b6
Lamaruggine <marcelolama.27@gmail.com>
2026-03-19 15:44:32 -0300

    Agregamos logica para que se puedan ver mas docentes y no todos

--- js/docentes.js
+++ js/docentes.js
@@ -1,4 +1,7 @@
 /* ===== Leemos el jason de docentes ===== */
+let listaDocentes = []; 
+let expandido = false; 
+
 
 fetch("data/docentes.json")
 .then(response => {
@@ -8,29 +11,60 @@ fetch("data/docentes.json")
     return response.json();
 })
 .then(docentes => {
-    console.log(docentes); 
-    mostrarDocentes(docentes); 
+    listaDocentes = docentes; 
+    mostrarDocentes();  
 })
 .catch(error => {
     console.error("Error: ", error); 
 }); 
 
-function mostrarDocentes(docentes){
-    const contenedorDocentes = document.querySelector("#contDocentes")
-    
-    docentes.forEach(docente => {
+const boton = document.getElementById("botonExpan"); 
+
+boton.addEventListener("click", function(e){
+    e.preventDefault(); 
+
+    expandido = !expandido; 
+    mostrarDocentes();
+
+    boton.textContent = expandido ? "Ver menos" : "Ver más"; 
+
+    if (!expandido) {
+        document.querySelector("#equipo").scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+    }
+})
+
+
+function mostrarDocentes(){
+    const contenedorDocentes = document.querySelector("#contDocentes");
+    contenedorDocentes.innerHTML = "";
+
+    const docentesAMostrar = expandido 
+        ? listaDocentes 
+        : listaDocentes.slice(0, 3);
+
+    docentesAMostrar.forEach((docente, index) => {
         let contenedor = document.createElement("div"); 
+        
+        contenedor.classList.add("tarjetaDocente");
+
         contenedor.innerHTML = `
-            <div class="cell tarjetaDocente imagen-wrapper">
+            <div class="imagen-wrapper">
                 <img class="imagenDocente" src="${docente.imagen}" alt="">
             </div>
-                <p class="nombreDocente">${docente.nombre}</p>
-                <p class="cargoDocente">${docente.cargo}</p>
-            
-        ` 
-        contenedorDocentes.appendChild(contenedor); 
+            <p class="nombreDocente">${docente.nombre}</p>
+            <p class="cargoDocente">${docente.cargo}</p>
+        `;
+
+        contenedorDocentes.appendChild(contenedor);
+
+        // ⏱️ Animación escalonada (clave tipo Netflix)
+        setTimeout(() => {
+            contenedor.classList.add("tarjetaDocente");
+        }, index * 100); // uno detrás de otro
     });
-    
 }
 
 
