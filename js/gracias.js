ed97512d606007555015db562e7146e4651d1c41
Lamaruggine <marceloandres.lamaruggine@telefonica.com>
2026-06-05 13:42:14 -0300

    agregamos la pantalla de gracias para el formulario

---
+++ js/gracias.js
@@ -0,0 +1,19 @@
+const form = document.querySelector("form");
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const data = new FormData(form);
+
+    const response = await fetch(form.action, {
+        method: "POST",
+        body: data,
+        headers: {
+            Accept: "application/json"
+        }
+    });
+
+    if (response.ok) {
+        window.location.href = "../pages/gracias.html";
+    }
+});
